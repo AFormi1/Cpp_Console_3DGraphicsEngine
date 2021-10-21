36c2c324c7a951a2e6bda65e59334b711a2d344c
Andreas Maier <i.andreas.maier@gmail.com>
2021-10-21 11:37:17 +0200

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Cpp_Console_3DGraphicsEngine
 Code-It-Yourself! 3D Graphics Engine Part #1 - Triangles &amp; Projection
+
+Tutorial 1:	https://www.youtube.com/watch?v=ih20l3pJoeU
+
---
+++ Source/Cpp_Console_3DGraphicsEngine/Cpp_Console_3DGraphicsEngine.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "###########################################" << endl;
+	cout << "#                                         #" << endl;
+	cout << "#     C++ Console 3D Graphics Engine      #" << endl;
+	cout << "#              MAA, 21.10.2021            #" << endl;
+	cout << "#                                         #" << endl;
+	cout << "###########################################" << endl;
+	cout << endl << endl;
+
+
+	string wait;
+	cin >> wait;
+
+}
